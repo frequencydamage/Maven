68914fa2b4306a86d958653c20806974f1b7db52
Andrej <andrej.novak.v@gmail.com>
2024-03-01 04:37:31 +0100

    main for task 4

---
+++ src/main/java/andersenCourse/task_4/Main.java
@@ -0,0 +1,8 @@
+package andersenCourse.task_4;
+
+public class Main {
+    public static void main(String[] args) {
+        //Task 4 - Написать программу, которая повторит действия на видео "Сценарий для автоматизации Лекция 10.mp4".
+        MethodRepeat.videoRepeat();
+    }
+}
